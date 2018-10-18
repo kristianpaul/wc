f12c0a34067c4c1707a7ee5f97c142a8bbf2050c
Cristian Paul Peñaranda Rojas <paul@kristianpaul.org>
2018-10-17 23:25:17 -0500

    method definition for counting data

--- wc.py
+++ wc.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 
-import re
-import os
 import sys
 import io
 
 # Argument parsing WIP
 
-if len(sys.argv) > 1:
-    print("usage: wc --help")
+def input_parse():
+    if len(sys.argv) > 1:
+        print("usage: wc --help")
+    
+    input=sys.stdin
 
-# Line parsing
+    # Read standard input as stream until EOF and get its len
+    input_read=input.read()
+    # Count new lines as in \n
+    input_lines=(input_read.count("\n"))
+    # Count new lines as in \n
+    input_spaces=(input_read.count(" "))
+    print (input_lines,end='\t')
+    print (input_spaces+input_lines,end='\t')
+    print (len(input_read),end='\n')
 
-input = sys.stdin
-
-# Read standard input as stream until EOF and get its len
-input_read=input.read()
-
-# Count new lines as in \n
-input_lines=(input_read.count("\n"))
-
-# Count new lines as in \n
-input_spaces=(input_read.count(" "))
-
-print (input_lines,end='\t')
-print (input_spaces+input_lines,end='\t')
-print (len(input_read),end='\n')
+input_parse()
