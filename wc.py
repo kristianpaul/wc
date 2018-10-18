48cf95297dc3965242fb804c7e215abbe37095dc
Cristian Paul Peñaranda Rojas <paul@kristianpaul.org>
2018-10-18 13:40:51 -0500

    Make module work as a script plus regex and variable re-work

--- wc.py
+++ wc.py
@@ -6,27 +6,30 @@ import re
 
 # Argument parsing WIP
 
-def input_parse():
+def argument_parse():
     if len(sys.argv) > 1:
         print("usage: wc --help")
-    
-    input=sys.stdin
 
+def input_parse(data_input):
+    
     # Read standard input as a stream until EOF and get its length 
-    input_read=input.read()
+    input_read=data_input.read()
     # Count new line character 
     count_of_lines=(input_read.count("\n"))
     # Count words from regex
-    count_of_words=len(re.findall('\w+', input_read))
+    count_of_words=len(re.findall('\S+', input_read))
     count_of_bytes=len(input_read)
 
     return count_of_lines,count_of_words,count_of_bytes
 
-def print_wc_like_count():
+def print_wc_like_count(data_input):
     try:
-        for  i in input_parse():
+        for  i in data_input:
             print(i,end='\t')
     finally:
         print('\n')
 
-print_wc_like_count()
+#Use module as an script
+if __name__ == '__main__':
+    data_input=sys.stdin
+    print_wc_like_count(input_parse(data_input))
