d43b959467a6d2baa50acd9c24c21c6ddd572413
Cristian Paul Peñaranda Rojas <paul@kristianpaul.org>
2018-10-31 00:33:26 -0500

    pytest single method testing

--- test_wc.py
+++ test_wc.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
-import unittest
 import io
-from wc import input_parse
+from wc import *
 
-class TestStringMethods(unittest.TestCase):
+def test_word_count():
+    test_in = io.StringIO("uno1 dos2 tres3\n")
+    a_wc = wc()
+    option = "w"
+    a_wc.input_parse(test_in)
+    assert a_wc.print_wc_like_count(option) == "3   \n\n"
 
-    def test_three_words(self):
-        test_in = io.StringIO("uno1 dos2 tres3\n")
-        self.assertEqual(input_parse(test_in), (1, 3, 16))
-
-    def test_three_words_two_lines(self):
-        test_in = io.StringIO("uno1 dos2 tres3\n.. cuatro  cinco\n")
-        self.assertEqual(input_parse(test_in), (2, 6, 33))
-
-    def test_three_linesi_and_words(self):
-        test_in = io.StringIO("uno1 dos2 tres3\n.. cuatro  cinco\nsix  7 9 ten. \n")
-        self.assertEqual(input_parse(test_in), (3, 10, 48))
-
-
-
-
-if __name__ == '__main__':
-    unittest.main()
